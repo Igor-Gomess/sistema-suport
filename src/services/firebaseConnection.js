8d92f4f8a92340afeeb317bd240f19bb9dc7a015
Igor Gomes <igorgomess@outlook.com>
2022-07-20 16:30:48 -0300

    first commit

--- src/services/firebaseConnection.js
+++ src/services/firebaseConnection.js
@@ -3,19 +3,7 @@ import 'firebase/auth';
 import 'firebase/firestore';
 import 'firebase/storage';
 
-let firebaseConfig = {
-    apiKey: "AIzaSyCKf9jny2UiZdILZNfRY8H1ACTh4c7bKoM",
-    authDomain: "sistema-1526c.firebaseapp.com",
-    projectId: "sistema-1526c",
-    storageBucket: "sistema-1526c.appspot.com",
-    messagingSenderId: "159133746155",
-    appId: "1:159133746155:web:89d6a8b797d4830060cfae",
-    measurementId: "G-56DLLDFG4Z"
-  };
-  
-  // Initialize Firebase
-  if(!firebase.apps.length){
-    firebase.initializeApp(firebaseConfig);
-  }
+// Aqui será introduzido autenticação de acesso para o banco de dados do Firebase
+
 
   export default firebase;
